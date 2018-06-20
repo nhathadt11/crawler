393cfc03d86997b5896580ba33029b406477db1b
Nhat Ha <nhathadt11@gmail.com>
2018-06-20 14:21:33 +0700

    Extract xml transformation logic to utility class

--- src/main/java/study/nhatha/spider/MovieDetailSpider.java
+++ src/main/java/study/nhatha/spider/MovieDetailSpider.java
@@ -5,20 +5,20 @@ import study.nhatha.middleware.ReplaceAllHandler;
 import study.nhatha.middleware.TransformerMiddleware;
 import study.nhatha.middleware.UnwantedClosingATagHandler;
 import study.nhatha.util.NetUtils;
-import study.nhatha.util.StringUtils;
+import study.nhatha.util.XmlUtils;
 
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static study.nhatha.util.AppConstants.HD_MOVIE_STYLE_SHEET;
 import static study.nhatha.util.NetUtils.toBufferedReader;
 import static study.nhatha.util.ResourceUtils.getResource;
+import static study.nhatha.util.StringUtils.normalize;
 import static study.nhatha.util.StringUtils.toInputStream;
 
 public class MovieDetailSpider implements Runnable {
@@ -37,7 +37,7 @@ public class MovieDetailSpider implements Runnable {
     System.out.println("GET / " + url);
     try (InputStream inputStream = NetUtils.connect(url)) {
       BufferedReader reader = toBufferedReader(inputStream);
-      String html = StringUtils.normalize(reader, "");
+      String html = normalize(reader, "");
 
       Pattern pattern = Pattern.compile(htmlFragmentExtractor);
       Matcher matcher = pattern.matcher(html);
@@ -52,17 +52,8 @@ public class MovieDetailSpider implements Runnable {
             .use(new ReplaceAllHandler("itemscope", ""))
             .apply();
 
-        TransformerFactory factory = TransformerFactory.newInstance();
-
         InputStream rulesIn = getResource(HD_MOVIE_STYLE_SHEET);
-        Transformer transformer = factory.newTransformer(new StreamSource(rulesIn));
-
-        OutputStream outputStream = new ByteArrayOutputStream();
-
-        transformer.transform(
-            new StreamSource(toInputStream(processed)),
-            new StreamResult(outputStream)
-        );
+        OutputStream outputStream = XmlUtils.transform(toInputStream(processed), rulesIn);
 
         System.out.println(outputStream.toString());
       }
--- src/main/java/study/nhatha/util/XmlUtils.java
+++ src/main/java/study/nhatha/util/XmlUtils.java
@@ -1,6 +1,26 @@
 package study.nhatha.util;
 
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public final class XmlUtils {
+
+  public static OutputStream transform(InputStream source, InputStream rulesIn)
+      throws TransformerException {
+    TransformerFactory factory = TransformerFactory.newInstance();
+    Transformer transformer = factory.newTransformer(new StreamSource(rulesIn));
+
+    OutputStream outputStream = new ByteArrayOutputStream();
+    transformer.transform(new StreamSource(source), new StreamResult(outputStream));
+
+    return outputStream;
+  }
 
   private XmlUtils() {
   }
